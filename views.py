c110f2b38c0c3f608672f238dcbc211a202cbc80
cricketlong <cricketlong@gmail.com>
2017-04-09 02:49:28 +0200

    initial commit

---
+++ views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Keyword
+
+def index(request):
+    if 'keyword' in request.POST:
+        keyword = request.POST['keyword']
+        results = Keyword.objects.filter(name__contains=keyword).select_related()
+    else:
+        results = None
+
+    keywords = Keyword.objects.select_related()
+    template = loader.get_template('keywords.html')
+    context = {
+        'keywords': keywords,
+        'results': results,
+    }
+
+    return HttpResponse(template.render(context, request))
